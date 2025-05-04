cf679c43d3d487029dcc953aca93d8e39a70bc3d
zackyrr7 <zackyrr7@gmail.com>
2025-05-04 16:16:43 +0700

    fix

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -59,51 +59,11 @@
                                 <p>{{ __('Pendapatan') }}</p>
                             </a>
                         </li>
-                        <li>
-                            <a href="{{ route('mobil.index') }}">
-                                <i class="tim-icons icon-bus-front-12"></i>
-                                <p>{{ __('Mobil Management') }}</p>
-                            </a>
-                        </li>
+
                     </ul>
                 </div>
             </li>
-            <li>
-                <a href="{{ route('pages.icons') }}">
-                    <i class="tim-icons icon-atom"></i>
-                    <p>{{ __('Icons') }}</p>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('pages.maps') }}">
-                    <i class="tim-icons icon-pin"></i>
-                    <p>{{ __('Maps') }}</p>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('pages.notifications') }}">
-                    <i class="tim-icons icon-bell-55"></i>
-                    <p>{{ __('Notifications') }}</p>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('pages.tables') }}">
-                    <i class="tim-icons icon-puzzle-10"></i>
-                    <p>{{ __('Table List') }}</p>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('pages.typography') }}">
-                    <i class="tim-icons icon-align-center"></i>
-                    <p>{{ __('Typography') }}</p>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('pages.rtl') }}">
-                    <i class="tim-icons icon-world"></i>
-                    <p>{{ __('RTL Support') }}</p>
-                </a>
-            </li>
+
 
         </ul>
     </div>
